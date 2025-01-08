93a3fbd2d9abe2cc5f1ffa6421e9231340272fcc
TijnLuckerhoff <luck0002@hz.nl>
2025-01-08 10:19:57 +0100

    Put in some helper tools to make the project easier

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,18 @@
-public class Main {
+import tools.ConsoleReader;
+import tools.ConsoleWriter;
 
+public class Main {
+    public static void main(String[] args) {
+        ConsoleReader reader = new ConsoleReader();
+        ConsoleWriter writer = new ConsoleWriter();
+
+        boolean quit = false;
+        while (!quit) {
+            String test = reader.readLine();
+            writer.writeLine(test);
+            if (test.equals("quit")) {
+                quit = true;
+            }
+        }
+    }
 }
