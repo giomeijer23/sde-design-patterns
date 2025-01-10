b5d8fa6d1dfbabee41c8020064a1107e6f739ae8
Gio Meijer <meij0155@hz.nl>
2025-01-10 11:35:24 +0100

    Verwijder code duplicatie

--- src/WinState.java
+++ src/WinState.java
@@ -13,8 +13,6 @@ public class WinState extends AHangmanState {
         this.writer.writeLine("You have guessed the word correctly! Do you want to play again?");
         String check = this.reader.readLine();
         if (check.equalsIgnoreCase("yes")) {
-            String wordToGuess = "hangman";
-            this.context.changeState(new PlayingState(wordToGuess));
             this.context.turn();
         } else {
             this.context.changeState(new EndState());
